cd73b1ef95726f74bc440600dfbd9a9e43f770e3
Roman Zabolotin <rzabolotin@gmail.com>
2022-10-16 02:35:48 +1000

    Homework #6

--- 5. make ml service/run_service.py
+++ 5. make ml service/run_service.py
@@ -1,6 +1,7 @@
 import requests
 
 url = 'http://localhost:8001'
+url = 'http://ml-churn-env.eba-bra9dhbe.us-east-1.elasticbeanstalk.com'
 
 client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
 
